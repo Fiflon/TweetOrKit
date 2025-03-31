6e80482dd056ee484eda3f204bc669d32768b31b
Fiflon <odzera.dotrofera@gmail.com>
2025-03-31 14:33:14 +0200

    Add remembering logged user

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tweetorkit/core/themes/main_theme.dart';
-import 'package:tweetorkit/features/auth/controllers/user_controller.dart';
 import 'package:tweetorkit/features/auth/screens/auth_screen.dart';
 import 'package:tweetorkit/features/home/screens/home_screen.dart';
 
@@ -21,11 +21,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TweetOrKit',
       theme: MainTheme.mainTheme, // Główny motyw aplikacji
-      initialRoute:
-          UserController.currentUser != null ? '/home' : '/', // Domyślna trasa
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // Jeśli trwa ładowanie stanu logowania
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          // Jeśli użytkownik jest zalogowany, przejdź do HomeScreen
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+
+          // Jeśli użytkownik nie jest zalogowany, przejdź do AuthScreen
+          return const AuthScreen();
+        },
+      ),
       routes: {
-        '/': (context) => const AuthScreen(), // Ekran logowania
         '/home': (context) => const HomeScreen(), // Ekran główny
+        //'/': (context) => const AuthScreen(), // Ekran logowania
       },
     );
   }
